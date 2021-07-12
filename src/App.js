8ffcfa7cc468a561e5f8b2d769dbac9aee1132e8
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-07-12 11:23:33 -0700

    todos update 3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,5 @@
 import React, { useReducer, useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
-
-const initialState = {
-    toDos: []
-}
-const ADD = "add";
-const DEL = "del";
-
-const reducer = (state, action) => {
-    switch (action.type) {
-        case ADD:
-            return { toDos: [ ...state.toDos, { text: action.payload, id: uuidv4() }] };
-        case DEL:
-            return {toDos: state.toDos.filter(toDo => toDo.id !== action.payload)};    
-        default:
-            return;
-    }
-};
+import reducer, { ADD, COMPLETE, DEL, initialState, UNCOMPLETE } from "./reducer";
 
 function App() {
     const [state, dispatch] = useReducer(reducer, initialState);
@@ -46,7 +29,23 @@ function App() {
                     <li key={toDo.id}>
                         <span>{toDo.text}</span>
                         <button onClick={() => dispatch({ type: DEL, payload: toDo.id})}>DEL</button>
+                        <button onClick={() => dispatch({ type: COMPLETE, payload: toDo.id})}>✅ </button>
                     </li>))}
+            </ul>
+            <ul>
+                {state.completed.length !== 0 && (
+                    <>
+                        <h2>Completed toDos</h2>
+                        {state.completed.map((toDo) => (
+                            <li key={toDo.id}>
+                                <span>{toDo.text}</span>
+                                <button onClick={() => dispatch({ type: DEL, payload: toDo.id})}>DEL</button>
+                                <button onClick={() => dispatch({ type: UNCOMPLETE, payload: toDo.id})}>🔙</button>
+                            </li>))
+                        }
+                    </>)
+                }
+                
             </ul>
         </>
     );
